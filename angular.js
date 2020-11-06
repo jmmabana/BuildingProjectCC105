2897f51e4909a072bf3c87cd6cc042d861ee584b
jmmabana <74047115+jmmabana@users.noreply.github.com>
2020-11-06 17:30:10 +0800

    Create angular.js

---
+++ angular.js
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+ <div class="container">
+ ...
+ </div>
+</nav>
+<div class="container" ng-controller="homeController">
+ ...
+ <footer class="footer">
+ <p>Copyright &copy; 2017</p>
+ </footer>
+</div>
